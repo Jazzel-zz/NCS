96b4689ff6e2bbd63d12041ba338f9bf0f190149
Dark_Shadow <farrukhahmed@gmail.com>
2019-12-29 20:45:07 +0500

    Add project files.

---
+++ NCS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCS.Startup))]
+namespace NCS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
